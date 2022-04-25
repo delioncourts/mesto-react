50c3c57589012270cb142be12f004097fe29c74d
delioncourts <delioncourtles@yandex.ru>
2022-04-26 00:22:38 +0300

    upd vendor

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-function Card(props) {
+function Card({card, onCardClick}) {
 
     function handleClick() {
-        props.onCardClick(props.card);
+        onCardClick(card);
     }
 
     return (
         <li className="card">
             <button className="card__delete" type="button" aria-label="удалить"></button>
-            <img className="card__image" src={props.card.link} alt={props.card.name} onClick={handleClick} />
+            <img className="card__image" src={card.link} alt={card.name} onClick={handleClick} />
             <div className="card__description">
-                <h2 className="card__title">{props.card.name}</h2>
+                <h2 className="card__title">{card.name}</h2>
                 <div className="card__container">
                     <button className="card__like" type="button" aria-label="нравится"></button>
-                    <div className="card__like-count">{props.card.likes.lenght}</div>
+                    <div className="card__like-count">{card.likes.lenght}</div>
                 </div>
             </div>
         </li>
